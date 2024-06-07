e197c860a83ed1da801af2c86aab23b8be71d12d
Kenil Shah <kenilshah42@gmail.com>
2024-06-08 01:13:57 +0530

    feat : line space

--- src/main/java/com/example/test/klevu/services/Search.java
+++ src/main/java/com/example/test/klevu/services/Search.java
@@ -1,1 +1,9 @@
-package com.example.test.klevu.services;import java.util.List;import java.util.Set;public interface Search {    public Set<String> findTerms(List<String> searchStrings);}
+package com.example.test.klevu.services;
+
+import java.util.List;
+import java.util.Set;
+
+public interface Search {
+
+    public Set<String> findTerms(List<String> searchStrings);
+}
